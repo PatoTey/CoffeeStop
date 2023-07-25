5dc6ab0449e95051105e31fddac86eda6b57579d
Teyo <73722247+PatoTey@users.noreply.github.com>
2023-07-24 23:39:53 -0400

    Agregada la carta del Dev en contacts y modificada la vista de historia del cafe!

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,4 +1,7 @@
 import React from "react";
+import uwu from "../images/uwu.png"
+import github from "../images/github.png"
+import linkedin from "../images/linkedin.png"
 
 export default function Contact(){
     return (
@@ -12,6 +15,18 @@ export default function Contact(){
                 </div>
                 <div className="contacts-phone">
                     Call (+9999) 2345 6789
+                </div>
+            </div>
+            <div className="devCard">
+                <img className="devAvatar" src={uwu} alt="El avatar del dev" />
+                <div className="devData">
+                    <h2>Patricio (Teyo) Tudela</h2>
+                    <p>FullStack Developer</p>
+                    <small>Arica, Chile</small>
+                    <div className="socials">
+                        <a target="_blank" rel="noreferrer" href="https://github.com/PatoTey"><img className="github" src={github} alt="el gato de github"/></a>
+                        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/patricio-tudela-chavera-4bab47268/"><img className="linkedin" src={linkedin} alt="iniciales de linkedin"/></a>
+                    </div>
                 </div>
             </div>
         </div>
